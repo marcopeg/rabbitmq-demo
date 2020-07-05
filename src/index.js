62f34c46185add1cb33854210c16770a50967361
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-07-05 10:39:20 +0000

    Improve on TDD

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const { runHookApp } = require('@forrestjs/hooks');
 const { settings } = require('./settings');
 
+/**
+ * Services
+ */
+
 const serviceFetchq = require('./service/service-fetchq');
 const serviceRabbitmq = require('./service/service-rabbitmq');
+const serviceFastify = require('./service/service-fastify');
+const serviceTdd = require('./service/service-tdd');
+
+/**
+ * Features
+ */
 
 const featurePocDelete = require('./feature/feature-poc-delete');
+
+/**
+ * Feature Flags
+ */
+
+const useDev =
+  ['development', 'test'].includes(process.env.NODE_ENV) ||
+  Boolean(process.env.SANDBOX_URL);
 
 runHookApp({
   settings,
   trace: 'compact',
-  services: [serviceFetchq, serviceRabbitmq],
+  services: [
+    serviceFetchq,
+    serviceRabbitmq,
+    ...(useDev ? [serviceFastify, serviceTdd] : []),
+  ],
   features: [featurePocDelete],
 }).catch(err => console.error(err.message));
 
