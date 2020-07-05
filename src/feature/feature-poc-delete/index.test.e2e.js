83988da1ca9574e9a26f7dc59b35aa6f6245f273
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-07-05 11:29:49 +0000

    Use TDD to send messages to the queue

--- src/feature/feature-poc-delete/index.test.e2e.js
+++ src/feature/feature-poc-delete/index.test.e2e.js
@@ -1,5 +1,33 @@
+const amqp = require('amqplib/callback_api');
+
+const queue = 'test1';
+const msg = '123';
+
 describe('feature-poc-delete', () => {
-  it('should work', () => {
-    console.log('it does');
+  it('should work', async () => {
+    await new Promise((resolve, reject) => {
+      amqp.connect(env.AMQPSTRING, (err, conn) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        conn.createChannel((err, channel) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          channel.assertQueue(queue, { durable: false });
+          channel.sendToQueue(queue, Buffer.from(msg));
+          console.log('Sent: %s', msg);
+
+          setTimeout(function() {
+            conn.close();
+            resolve();
+          }, 500);
+        });
+      });
+    });
   });
 });
